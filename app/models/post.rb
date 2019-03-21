1a9aeb582a3eeabbcaabbd5c2b26ba6e2d17b5ac
max-powell <github email address>
2019-03-21 22:27:54 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,19 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
+  
 
+# def categories_attributes=(category_attributes)
+#   category_attributes.each do |attribute|
+#     byebug
+#     category = Category.find_or_create_by(attribute)
+#     self.categories << category
+#   end
+# end
+
+def commenters
+  self.comments.map(&:user).uniq
+end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  def commented_posts
+    self.comments.map(&:post).uniq
+  end
 end
